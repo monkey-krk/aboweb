e6c4fea9f0e6ad7dca934750387a859372cfba23
monkey-krk <monkey@monkey.com.hr>
2014-10-23 19:48:35 +0200

    Index.html

---
+++ Front/Assets/Js/Index.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="../External/jQuery/jquery-2.1.1.min.js" />
+
+$(document).ready(function () {
+
+    // TopBanner Image Height
+    var WindowHeight = $(window).height();
+    if (WindowHeight > 520) {
+        $(".TopBanner .ImageBox").css("height", WindowHeight);
+        $(".TopBanner .ImageBox  > div").css("height", WindowHeight);
+        $(".TopBanner .ImageOverBox").css("height", WindowHeight);
+    }
+
+    //TopBanner Image Blur Effect
+    $(window).scroll(function () {
+        oVal = ($(window).scrollTop() / ($(window).height() / 2.2));
+        $('.ImageBox .blur').css('opacity', oVal);
+    });
+
+    //Reminders Slider
+    if ($(".RemindersSlider").length > 0) {
+        $(".RemindersSlider .Slide").mouseenter(function (e) {
+            if (!$(this).hasClass("active")) {
+                var slideno = $(this).attr("data-slideno");
+                ShowSlide(slideno);
+            }
+        });
+        $(".Reminders .Reminder").click(function (e) {
+            var slideno = $(this).attr("data-slideno");
+            ShowSlide(slideno);
+            e.preventDefault();
+        });
+        function ShowSlide(slideno) {
+            $(".RemindersSlider .Slide").removeClass("active");
+            $(".RemindersSlider .Slide .Title").removeClass("active").hide();
+            $(".Reminders .Reminder").removeClass("active");
+            var Slide = $('.RemindersSlider .Slide[data-slideno="' + slideno + '"]');
+            var Span = $('.RemindersSlider .Slide[data-slideno="' + slideno + '"] .Title');
+            var Button = $('.Reminders .Reminder[data-slideno="' + slideno + '"]');
+
+            Slide.addClass("active");
+            Button.addClass("active");
+            Span.fadeIn(500);
+            Span.addClass("active");
+
+        }
+    }
+
+      
+
+
+
+
+
+});
---
+++ Front/Assets/Js/Shared.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+
+    //Header Transform
+    $(function () {
+        $('.MainHeader').data('size', 'big');
+    });
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 0) {
+            if ($('.MainHeader').data('size') == 'big') {
+                $('.MainHeader').data('size', 'small');
+                $('.MainHeader').fadeOut(250, function () {
+                    $('.MainHeader').addClass("sticky").css("margin-top", "0px");
+                    $(".LogoImage").attr("src", "/Assets/Img/LogoBlack.png");
+                    $('.MainHeader').fadeIn(250);
+                });
+            }
+        }
+        else {
+            if ($('.MainHeader').data('size') == 'small') {
+                $('.MainHeader').data('size', 'big');
+                $('.MainHeader').fadeOut(250, function () {
+                    $('.MainHeader').removeClass("sticky").css("margin-top", "10px");
+                    $(".LogoImage").attr("src", "/Assets/Img/LogoWhite.png");
+                    $('.MainHeader').fadeIn(250);
+                });
+            }
+        }
+    });
+
+    //Triger element animation
+    $(window).scroll(function (event) {
+        $(".animated").each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("visible");
+            }
+        });
+    });
+
+});
